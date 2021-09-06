d53fd369035cecb37c9a2e66f28c0d0d7fff82fb
libaice <libaice147@gmail.com>
2021-09-06 10:19:25 +0800

    application.yml add  update-cycle-time : 3000

---
+++ src/main/java/com/github/ontio/explorer/statistics/mapper/NodeCycleMapper.java
@@ -0,0 +1,28 @@
+package com.github.ontio.explorer.statistics.mapper;
+
+import com.github.ontio.explorer.statistics.model.NodeCycle;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+@Repository
+public interface NodeCycleMapper extends Mapper<NodeCycle> {
+
+    int batchSave(@Param("nodeCycleList") List<NodeCycle> nodeCycleList);
+
+    int selectMaxCycle();
+
+    List<String> selectLastPublicKeys(Integer cycle);
+
+    List<NodeCycle> selectCycleData(Integer cycle);
+
+    // 根据publicKey,更新库中上次数据的node_proportion_t2和user_proportion_t2
+    int updateLastCycleProportion(@Param("publicKey") String publicKey, @Param("cycle") Integer cycle, @Param("nodeProportionT2") String nodeProportionT2, @Param("userProportionT2") String userProportionT2);
+
+    int selectCycleNodeCount(@Param("nodeType") Integer nodeType, @Param("cycleNum") Integer cycleNum);
+
+    int updateBonusOng(@Param("bonusOng") Double bonusOng, @Param("pubKey") String pubKey, @Param("cycleNum") Integer cycleNum);
+
+}
