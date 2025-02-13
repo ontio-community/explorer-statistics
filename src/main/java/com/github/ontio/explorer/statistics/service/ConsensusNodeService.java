d9817c2b8545b26a2fefa5cb54afaeaf7e212db6
leej1012 <jlee921012@163.com>
2025-02-13 14:19:48 +0800

    Update node DetailUrl

--- src/main/java/com/github/ontio/explorer/statistics/service/ConsensusNodeService.java
+++ src/main/java/com/github/ontio/explorer/statistics/service/ConsensusNodeService.java
@@ -367,7 +367,7 @@ public class ConsensusNodeService {
             }
             node.setCurrentStake(currentPos.longValue());
             node.setProgress(progress.toPlainString() + "%");
-            node.setDetailUrl(paramsConfig.getConsensusNodeDetailUrl() + node.getPublicKey());
+            node.setDetailUrl(paramsConfig.getConsensusNodeDetailUrl() + node.getPublicKey() + "/" + node.getAddress() + "/balance");
             BigDecimal percent = new BigDecimal(node.getCurrentStake()).multiply(new BigDecimal(100)).divide(new BigDecimal(1000000000), 4, RoundingMode.HALF_UP);
             node.setCurrentStakePercentage(percent.toPlainString().concat("%"));
             nodes.set(i, node);
